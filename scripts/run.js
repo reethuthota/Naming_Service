574246e524287e0e98dbd2df205ecc6038433deb
reethuthota <reethu.thota@gmail.com>
2023-11-19 19:47:06 +0530

    Update domains.sol
    Storing domain data on-chain

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,17 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
-    //await domainContract.deployed();
-    console.log("Contract deployed to:", await domainContract.getAddress());
-  };
+    //await domainContract.deployed(); no need to used .deployed()
+    console.log("Contract deployed to:", await domainContract.getAddress()); // use this .getAddress()
+    console.log("Contract deployed by:", owner.address);
+    
+    const txn = await domainContract.register("doom");
+    await txn.wait();
+  
+    const domainOwner = await domainContract.getAddress("doom");
+    console.log("Owner of domain:", domainOwner);
+  }
   
   const runMain = async () => {
     try {
