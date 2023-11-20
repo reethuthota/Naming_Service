516f623e35c5192854dd36605e4c7968d2e2265a
reethuthota <reethu.thota@gmail.com>
2023-11-20 16:16:56 +0530

    Update deploy.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    mumbai: {
+      url: "https://omniscient-intensive-paper.matic-testnet.quiknode.pro/238725fb03cc7f2f6a06dcdbc8dbba293568e6ea/",
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    }
+  }
 };
