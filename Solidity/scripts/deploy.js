8dd4355288c56e3e8aad4a1b4b65a897e8002316
reethuthota <reethu.thota@gmail.com>
2023-11-25 13:04:14 +0530

    Update deploy.js

--- Solidity/scripts/deploy.js
+++ Solidity/scripts/deploy.js
@@ -1,22 +1,21 @@
 const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy("ninja");
+    const domainContract = await domainContractFactory.deploy("CryptoConnect");
     //await domainContract.deployed();
   
     console.log("Contract deployed to:", await domainContract.getAddress());
   
-    // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
-    let txn = await domainContract.register("mango",  {value: hre.ethers.parseEther('0.1')});
+    let txn = await domainContract.register("cloak",  {value: hre.ethers.parseEther('0.1')});
     await txn.wait();
-    console.log("Minted domain mango.ninja");
+    console.log("Minted domain cloak.CryptoConnect");
   
-    txn = await domainContract.setRecord("mango", "Am I a mango or a ninja??");
+    txn = await domainContract.setRecord("cloak", "invisibility??");
     await txn.wait();
-    console.log("Set record for mango.ninja");
+    console.log("Set record for cloak.CryptoConnect");
     
   
-    const address = await domainContract.getAddress("mango");
-    console.log("Owner of domain mango:", address);
+    const address = await domainContract.getAddress("cloak");
+    console.log("Owner of domain cloak:", address);
   
     const balance = await hre.ethers.provider.getBalance(domainContract.getAddress());
     console.log("Contract balance:", hre.ethers.formatEther(balance));
@@ -33,3 +32,5 @@ const main = async () => {
   };
   
   runMain();
+
+//npx hardhat run scripts/deploy.js --network mumbai
